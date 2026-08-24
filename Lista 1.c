1ebac5da742d66afbee4477cb1b6aa2419edb308
Eduardo França <esmfranc456@gmail.com>
2026-08-24 19:48:54 -0300

    Update Lista 1.c

--- Lista 1.c
+++ Lista 1.c
@@ -16,11 +16,12 @@ int main() {
 	B = C;
 	printf("Os valores são: %d e %d", A, B);
 	
-	printf("\n---------------\n| Exercício 2 |\n---------------\n");
-	double D;
-	printf("Escreva o valor: ");
-	scanf("%lf", &D);
-	printf("Seu valor: %.1lfx10", D);
+	printf("\n------------EXERCICIO 2------------\n");
+	float K, M;
+	printf("\nQual a velocidade de em km/h: \n");
+	scanf("%f", &K);
+	M = K/36;
+	printf("%.1f km/h convertido em metros e %.2f m/s",K,M);
 	
 	printf("\n---------------\n| Exercício 3 |\n---------------\n");
 	int D, res, b64, b32, b16, b8, b4, b2, b1;
@@ -40,7 +41,54 @@ int main() {
 	res = res/2;
 	printf("O binário de %d é: %d%d%d%d%d%d%d", D, res%2, b2, b4, b8, b16, b32, b64);
 
-	printf("O binário de %d é: %d%d%d%d%d%d%d", D, res%2, b2, b4, b8, b16, b32, b64);
+	printf("\n---------------\n| Exercício 4 |\n---------------\n");
+	float sal, vendas, comissao, total;
+    printf("Digite o seu salário: ");
+    scanf("%f", &sal);
+    printf("Digite o valor total de vendas: ");
+	scanf("%f", &vendas);
+    comissao=vendas*0.15;
+    if (vendas > 0){
+     total = sal + comissao;
+     printf("Seu salario e de %.2f, a boni doida e %.2f.\n TOTAL: R$%.2f\n", sal, comissao, total);
+    }else{
+        printf("Seu salario e R$%.2f e você nao teve nenhuma comissao!\n", sal);
+    }
+	
+	printf("\n---------------\n| Exercício 5 |\n---------------\n");
+	int n1, n2, n3, n4, soma, media, produ;
+    printf("Digite o primeiro numero: ");
+    scanf("%d", &n1);
+	printf("Digite o segundo numero: ");
+    scanf("%d", &n2);
+	printf("Digite o terceiro numero: ");
+    scanf("%d", &n3);
+    printf("Digite o quarto numero: ");
+    scanf("%d", &n4);
+    soma = n1 + n2 + n3 + n4;
+    printf("A soma entre %d + %d + %d + %d é igual a: %d\n", n1, n2, n3, n4, soma);
+    media = soma/4;
+    printf("A media entre os numeros escolhidos e %d\n", media);
+    produ = n1*n2*n3*n4;
+    printf("O produtorio e %d\n", produ);
+	
+	printf("\n---------------\n| Exercício 6 |\n---------------\n");
+	int ano, meses, dias;
+    printf("Digite sua idade: ");
+    scanf("%d", &ano);
+    meses = ano*12;
+    dias = ano*365;
+	printf("Sua idade em anos: %d\n", ano);
+	printf("Sua idade em meses: %d\n", meses);
+    printf("Sua idade em dias: %d\n", dias);
+
+	printf("\n---------------\n| Exercício 7 |\n---------------\n");
+	float raio, r3, volume;
+    printf("Digite o raio: ");
+    scanf("%f", &raio);
+    r3 = pow(raio, 3);
+    volume = (4/3.0) * pi * r3;
+    printf("O volume da esfera é: %.2f\n", volume);
 	
 	printf("\n---------------\n| Exercício 8 |\n---------------\n");
 	int x1, x2, y1, y2, p1, p2;
